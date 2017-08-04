f7ed5eb5bd189da6753b2248831cd101069f6113
MarcoVMontoya <marcovmontoya@gmail.com>
2017-08-04 00:13:45 -0500

    templates
    Templates

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -1,11 +1,22 @@
 angular.module('TemplateStore.templates',['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/templates',{
+    $routeProvider.
+    when('/templates',{
         templateUrl: 'templates/templates.html',
         controller: 'TemplateCtrl'
-    });
+        }).
+    when('/templates/:id',{
+            templateUrl:'templates/template-detail.html',
+            controller: 'TemplateDetail'
+        })
+
+    ;
 }])
 .controller('TemplateCtrl',['$scope', function ($scope) {
     $scope.name = 'view templates   ';
     console.log('TemplateCtrl init');
+}])
+    .controller('TemplateDetail',['$scope', function ($scope) {
+    $scope.name = 'view templates detail  ';
+    console.log('TemplateDetail init');
 }]);
