44409683a04d4e4d935ae1b77e6f4ec4e193e7d1
Marco Antonio V Montoya Cardenas <marco.montoya@linio.com>
2017-08-14 13:13:04 -0500

    firebase base template

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,23 @@ angular.module('myContact.contacts', ['ngRoute', 'firebase'])
   });
 }])
 
-.controller('ContactCtrl', ['$scope', '$firebaseArray',function($scope, $firebaseArray) {
+.controller('ContactCtrl', ['$scope','$firebaseObject',function($scope, $firebaseObject) {
   console.log($scope);
+    var ref = firebase.database().ref().child('business');
+    // download the data into a local object
+    $scope.data = $firebaseObject(ref);
+    // putting a console.log here won't work, see below
+    console.log($scope.data);
+    
+    $scope.addnewShow = function () {
+      $scope.addFormShow = true;
+      $scope.data.$add({
+
+      });
+    };
+
+    $scope.addMessage = function() {
+        $scope.messages.$add({
+            text: $scope.newMessageText
+        });
 }]);
